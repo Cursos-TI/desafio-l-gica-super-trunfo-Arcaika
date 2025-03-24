1af78ff48f1655e3a52edea35c6749cf407c27cd
Sara <sara.rosa.sr44@gmail.com>
2025-03-24 17:08:16 +0000

    Estrutura Switch concluída

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -14,15 +14,17 @@ int main() {
     float Pib1;
     int PontosTuristicos1;
     float Area1;
+    float Densidade1;
     
    /*--------------------------------------*/
     char Estado2 [20];
-    char Codigo3 [4];  
-    char Nome3 [20];
-    int Populacao3;
-    float Pib3;
-    int PontosTuristicos3;
-    float Area3;
+    char Codigo2 [4];  
+    char Nome2 [20];
+    int Populacao2;
+    float Pib2;
+    int PontosTuristicos2;
+    float Area2;
+    float Densidade2;
 
     
     // Cadastro das Cartas:
@@ -41,23 +43,28 @@ int main() {
    scanf("%d", &PontosTuristicos1);
    printf("Digite a área (em km²): \n");
    scanf("%f", &Area1);
+   printf("Digite a densidade (hab/km²): \n");
+   scanf("%f", &Densidade1);
      
    printf("\n----------------------------------\n"); /*Para melhor organização*/
 
    printf("Digite outro estado:\n"); /*Estado 2*/
    scanf("%s", Estado2);
    printf("Digite o código da cidade: \n");
-   scanf("%s", Codigo3);
+   scanf("%s", Codigo2);
    printf("Digite o nome da cidade: \n");
-   scanf(" %[^\n]", Nome3);
+   scanf(" %[^\n]", Nome2);
    printf("Digite o número da populaçao: \n");
-   scanf("%d", &Populacao3);
+   scanf("%d", &Populacao2);
    printf("Digite o Pib: \n");
-   scanf("%f", &Pib3);
+   scanf("%f", &Pib2);
    printf("Digite o número de pontos turísticos: \n");
-   scanf("%d", &PontosTuristicos3);
+   scanf("%d", &PontosTuristicos2);
    printf("Digite a área (em km²): \n");
-   scanf("%f", &Area3);
+   scanf("%f", &Area2);
+   printf("Digite a densidade (hab/km²): \n");
+   scanf("%f", &Densidade2);
+     
    printf("\n----------------------------------\n"); /*Para melhor organização*/
 
 
@@ -72,18 +79,55 @@ printf("5-Comparação de Densidade\n");
 
     switch (Opçoes){
     case 1:
-    if (Populacao1>Populacao3){
+    if (Populacao1>Populacao2){
     printf("Carta 1 venceu!\n");
-    }else if (Populacao1<Populacao3){
+    }else if (Populacao1<Populacao2){
     printf("Carta 2 venceu!\n");
-        /* code */
+    }else{
+    printf("Ocorreu empate!\n");
     }
-    
-
     break;
-
+    case 2:
+    if (Pib1>Pib2){
+        printf("Carta 1 venceu!\n");
+        }else if (Pib1<Pib2){
+        printf("Carta 2 venceu!\n");
+        }else{
+        printf("Ocorreu empate!\n");
+        }
+    break;
+    case 3:
+    if (PontosTuristicos1>PontosTuristicos2){
+        printf("Carta 1 venceu!\n");
+        }else if (PontosTuristicos1<PontosTuristicos2){
+        printf("Carta 2 venceu!\n");
+        }else{
+        printf("Ocorreu empate!\n");
+        }
+    break;    
+    case 4:
+    if (Area1>Area2){
+        printf("Carta 1 venceu!\n");
+        }else if (Area1<Area2){
+        printf("Carta 2 venceu!\n");
+        }else{
+        printf("Ocorreu empate!\n");
+        }
+    break;
+    case 5:
+    if (Densidade1>Densidade2){
+        printf("Carta 1 venceu!\n");
+        }else if (Densidade1<Densidade2){
+        printf("Carta 2 venceu!\n");
+        }else{
+        printf("Ocorreu empate!\n");
+        }    
+    break;    
 default:
+printf("Opção inválida!\n");
     break;
+    
+return 0;    
 }
     // Exemplo:
     // if (populacaoA > populacaoB) {
